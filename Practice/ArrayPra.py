e5bfa6315cc028c207ea2dff3f017a6f4e3e8bd1
Neel Patel <neelpatel000700@gmail.com>
2026-08-04 18:52:38 +0530

    String

--- Practice/ArrayPra.py
+++ Practice/ArrayPra.py
@@ -21,8 +21,35 @@
 # 2 mango
 
 
-# Loop through multiple arrays simultaneously with unpacking
-nums1 = [1, 3, 5]
-nums2 = [2, 4, 6]
-for n1, n2 in zip(nums1, nums2):
-    print(n1, n2)
+# # Loop through multiple arrays simultaneously with unpacking
+# nums1 = [1, 3, 5]
+# nums2 = [2, 4, 6]
+# for n1, n2 in zip(nums1, nums2):
+#     print(n1, n2)
+
+
+# # Sorting
+# arr = [5, 4, 7, 3, 8]
+# arr.sort()
+# print(arr)
+
+# arr.sort(reverse=True)
+# print(arr)
+
+
+# **** Rule of thumb: If the function is just one simple expression, lambda is a good choice. If it needs multiple statements, loops, or conditions, use def.
+# arr = ["me", "you", "he", "she", "it"]
+# print(arr)
+
+# arr.sort(key =lambda x: len(x))
+# print(arr)
+
+
+### Array multiplication
+# a1 = [1, 2, 3]
+# a2 = [4, 5, 6]
+
+# for i in a1:
+#     print("\n")
+#     for j in a2:
+#         print(i * j)
