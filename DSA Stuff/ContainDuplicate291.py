f81f5a11f8e846e7e4819bb1a24150ab0a9f9450
Neel Patel <neelpatel000700@gmail.com>
2026-08-15 16:51:06 +0530

    Solved

--- DSA Stuff/ContainDuplicate291.py
+++ DSA Stuff/ContainDuplicate291.py
@@ -1,12 +1,30 @@
-class Solution:
-    def hasDuplicate(self, nums):
-        count = 0
+#not fisible:
 
-        for i in range(1, len(nums) + 1):
-            for j in range(i):
-                print(nums[j], end=" ")
-            print()
+# class Solution:
+#     def hasDuplicate(self, nums):
+        # nums.sort()
+# #         for i in range(1, len(nums)):
+#             if nums[i] == nums[i - 1]:
+#                 return True
+#             return False
 
-sol = Solution()
-list = [10,2,3,4]
-sol.hasDuplicate(list)
+# sol = Solution()
+# list = [10,2,10,3,4, 10]
+# sol.hasDuplicate(list)
+
+
+
+#besdt and optimal
+#HashSet solution
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         seen = set() # tracks every digit we have seen
+        
+#         for num in nums:
+#             if num in seen:
+#                 return True
+#             else:
+#                 seen.add(num)
+            
+#         return False
